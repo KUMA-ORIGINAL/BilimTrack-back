8db5871027f71c139b54f709cc4974d72880d5e0
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-02-12 18:29:36 +0600

    refact: grade

--- app/academics/signals.py
+++ app/academics/signals.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+
+from account.models import User
 from .models import Grade
 
 
@@ -22,18 +24,15 @@ def update_user_and_group_points(grade_instance):
     user.save()
 
     # Обновляем рейтинг пользователя
-    update_user_rating(user)
+    update_user_rating()
 
     group = user.group
     if group:
         group.update_group_points()
 
-def update_user_rating(user):
-    all_users = user.__class__.objects.order_by('-points')
+def update_users_rating():
+    all_users = User.objects.order_by('-points')
 
-    # Присваиваем рейтинг пользователю, основываясь на его позиции в списке
-    for rank, u in enumerate(all_users, 1):
-        if u == user:
-            user.rating = rank
-            user.save()
-            break
+    for rank, user in enumerate(all_users, 1):
+        user.rating = rank
+        user.save()
