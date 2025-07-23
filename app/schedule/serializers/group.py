14b1fa33d0b2242a284f84d5ff005ba940d4bc33
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-07-23 15:27:50 +0600

    feat: add session

--- app/schedule/serializers/group.py
+++ app/schedule/serializers/group.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from academics.models import Group
+from schedule.serializers import CourseSerializer
 
 
 class GroupSerializer(serializers.ModelSerializer):
+    course = CourseSerializer(read_only=True)
+
     class Meta:
         model = Group
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'course']
