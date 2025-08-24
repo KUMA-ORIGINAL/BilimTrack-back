48fc19e4f13c522b62b5d715540aed42b03544e6
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-08-24 20:02:38 +0600

    feat: add session

--- app/academics/models/session.py
+++ app/academics/models/session.py
@@ -5,7 +5,7 @@ from django.db import models
 class Session(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     subject = models.ForeignKey('Subject', on_delete=models.CASCADE, verbose_name='Предмет')
-    topic = models.ForeignKey('LessonTopic', on_delete=models.CASCADE, verbose_name="Тема урока", blank=True, null=True)
+    topic = models.ForeignKey('LessonTopic', on_delete=models.SET_NULL, verbose_name="Тема урока", blank=True, null=True)
     date = models.DateField(verbose_name='Дата занятия')
     start_time = models.TimeField(verbose_name='Начало занятия')
     end_time = models.TimeField(verbose_name='Конец занятия')
