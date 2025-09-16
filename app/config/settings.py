d94665801bb64fc0c3a0410f1ea6e84f348b11ae
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-09-16 18:44:29 +0600

    feat: add session

--- app/config/settings.py
+++ app/config/settings.py
@@ -301,7 +301,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "AUTH_HEADER_TYPES": ("Bearer",),
     'UPDATE_LAST_LOGIN': True,
