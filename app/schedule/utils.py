b10069f22d800b413d166093b8a3467ef7792c35
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-09-16 11:47:31 +0600

    feat: add session

--- app/schedule/utils.py
+++ app/schedule/utils.py
@@ -4,5 +4,9 @@ import datetime
 def get_week_type(date=None):
     if date is None:
         date = datetime.date.today()
-    week_number = date.isocalendar()[1]  # номер недели
-    return 'top' if week_number % 2 else 'bottom'
+
+    # Определяем номер дня месяца и вычисляем неделю
+    week_of_month = (date.day - 1) // 7 + 1
+
+    # нечетные недели -> top, четные -> bottom
+    return 'top' if week_of_month % 2 == 1 else 'bottom'
