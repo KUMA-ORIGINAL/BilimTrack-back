5c9a4df6803fde95e47a3ae95c9e98458860be48
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-09-23 17:32:59 +0600

    feat: add session

--- app/app_entrypoint.prod.sh
+++ app/app_entrypoint.prod.sh
@@ -4,4 +4,4 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
-gunicorn config.wsgi --workers 2 --bind 0.0.0.0:8000 --timeout 60 --log-level info
+gunicorn config.wsgi --workers 2 --bind 0.0.0.0:8000 --timeout 300 --log-level info
